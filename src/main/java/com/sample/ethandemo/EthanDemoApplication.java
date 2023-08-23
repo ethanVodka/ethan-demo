72917020d3b9a64b8954ccca2fa2e975d235dcc5
ethan <gohho1008@gmail.com>
2023-08-23 12:22:22 +0900

    init spring project

---
+++ src/main/java/com/sample/ethandemo/EthanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sample.ethandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthanDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthanDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/ethandemo/EthanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.ethandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthanDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
